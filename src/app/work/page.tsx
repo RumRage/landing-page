4f9d0fb2e8c3eab54adfd30afe084a8ae84ed686
rumrage <claravillarruel4@gmail.com>
2024-02-04 19:27:25 -0300

    first version mobile photos

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import "../../styles/globals.scss";
+import Link from "next/link";
 
 export default function Work() {
   return (
     <main className="container">
-      <div className="background_photo">
+      <div className="second_section">
         {/* TEXT */}
         <h1 className="title_work">Funcionamiento</h1>
         <p className="subtitle">
@@ -20,8 +22,34 @@ export default function Work() {
           </ol>
         </div>
         {/* IMAGE */}
-        <div>
+        <div className="image_container">
           <h1 className="title_image">Manejado por agentes AI y humanos</h1>
+          <div className="image_wrapper">
+            <Image
+              src="/assets/agents.png"
+              alt="agents"
+              width={250}
+              height={200}
+            />
+          </div>
+          <Link href="#" className="link">
+            ...Ver Agentes
+          </Link>
+        </div>
+        <div className="complete">
+          <h1 className="secondary_title">Esquema</h1>
+          <Image
+            src="/assets/graph2.png"
+            alt="Graph Mobile"
+            width={100}
+            height={500}
+            layout="responsive"
+          />
+        </div>
+        <div className="container_bottom">
+          <div className="button_container">
+            <button className="info_button">Más información →</button>
+          </div>
         </div>
       </div>
     </main>
