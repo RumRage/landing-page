68baa608e00e0ef376d8e078d80c35f04c8e1137
rumrage <claravillarruel4@gmail.com>
2024-02-04 13:53:50 -0300

    home text

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,11 +4,20 @@ export default function Home() {
   return (
     <main className="background-container container">
       <div className="section">
+        {/* SLIDER */}
         <div>
           <h1>Slider</h1>
         </div>
+        {/* TEXT */}
         <div>
-          <h1>Home</h1>
+          <h1>Comunicación Inteligente</h1>
+          <h5>
+            Lleva la comunicación con tus clientes al siguiente nivel
+            <b> todo-en-uno</b>
+          </h5>
+          <p>&gt; Crea campañas de alto impacto</p>
+          <p>&gt; Gestiona las consultas omnicanal</p>
+          <p>&gt; Potencia tus chatbots corporativos.</p>
         </div>
       </div>
     </main>
