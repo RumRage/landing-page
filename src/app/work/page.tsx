1616ecc6e512e3492b1305fcbd1b6ec09a1a9fe0
rumrage <claravillarruel4@gmail.com>
2024-02-06 19:42:45 -0300

    fix button section mobile

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -7,10 +7,10 @@ import Banner from "@/components/Banner";
 export default function Work() {
   return (
     <div className="work_section">
-      <div className="background_nuts"></div>
-      {/* TEXT */}
+      <div className="top_background_nuts"></div>
+      {/* WORK */}
       <div className="container">
-        <div className="first_section">
+        <div className="text_section">
           <h1 className="title_work">Funcionamiento</h1>
           <p className="subtitle">
             Envío de mensaje por Whatsapp, SMS o correo con enlace único.
@@ -27,49 +27,49 @@ export default function Work() {
             </ol>
           </div>
         </div>
-        {/* IMAGE */}
-        <div className="image_section">
-          <h1 className="title_image">Manejado por agentes AI y humanos</h1>
+        {/* AGENTS */}
+        <div className="agents_section">
+          <h1 className="title_agents">Manejado por agentes AI y humanos</h1>
           {/* Mobile Image */}
           <Image
             src="/assets/agents.png"
-            alt="agents"
+            alt="Agents"
             width={202}
             height={155}
-            className="mobile_image"
+            className="mobile_image_agents"
           />
 
           {/* Desktop Image */}
           <Image
             src="/assets/agents.png"
-            alt="agents"
+            alt="Agents"
             width={350}
             height={270}
-            className="desktop_image"
+            className="desktop_image_agents"
           />
           <Link href="#" className="link">
             ...Ver Agentes
           </Link>
         </div>
       </div>
-      {/* 
-      <div className="image_graph">
-        <h1 className="secondary_title">Esquema</h1>
+
+      {/* DIAGRAM */}
+
+      <div className="image_section">
+        <h1 className="title">Esquema</h1>
         <Image
           src="/assets/graph2.png"
           alt="Graph Mobile"
           width={329}
           height={777}
         />
-
-        <div className="button_container">
-          <button className="info_button">Más información →</button>
-        </div>
-        <div className="second_background_nuts"></div>
       </div>
+      <div className="button_container">
+        <button>Más información →</button>
+      </div>
+      <div className="bottom_background_nuts"> </div>
 
       <Banner />
-      */}
     </div>
   );
 }
