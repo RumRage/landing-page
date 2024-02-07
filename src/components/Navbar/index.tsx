72b89fa332e8e9e13f0607258413eb3a92b23560
rumrage <claravillarruel4@gmail.com>
2024-02-07 00:10:07 -0300

    Add new feature navbar desktop

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,67 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./styles.module.scss";
 import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <Image src="/icons/image 4.svg" alt="Logo" width={183} height={36} />
+      {/* MOBILE NAVBAR */}
+
+      <div className={styles.mobile_navbar}>
+        <div className={styles.logo}>
+          <Image src="/icons/image 4.svg" alt="Logo" width={183} height={36} />
+        </div>
+        <div>
+          <button className={styles.menuButton}>
+            <FaBars className={styles.menuIcon} />
+          </button>
+        </div>
       </div>
-      <div>
-        <button className={styles.menuButton}>
-          <FaBars className={styles.menuIcon} />
-        </button>
+
+      {/* DESKTOP NAVBAR */}
+      <div className={styles.desktop_navbar}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image
+              src="/icons/image 4.svg"
+              alt="Logo"
+              width={183}
+              height={36}
+            />
+          </Link>
+        </div>
+        <div className={styles.nav_group}>
+          <div onClick={toggleDropdown} className={styles.dropdown}>
+            Productos ▼
+            {dropdownOpen && (
+              <div className={styles.dropdown_content}>
+                <Link href="/">Chatbots</Link>
+                <Link href="/">Agents</Link>
+                <Link href="/">Agents</Link>
+              </div>
+            )}
+          </div>
+
+          <Link href="/">Pricing</Link>
+          <Link href="/">Documentación</Link>
+        </div>
+        <div className={styles.nav_about}>
+          <Link href="/">Hablar con un asesor</Link>
+          <button className={styles.button_notimation}>
+            Notimation Entreprice
+          </button>
+          <button className={styles.button_languaje}>ES</button>
+        </div>
       </div>
     </nav>
   );
