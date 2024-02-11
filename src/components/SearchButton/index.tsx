a39769042f75bc27c90b785231363e1d9150e325
rumrage <claravillarruel4@gmail.com>
2024-02-11 12:49:48 -0300

    fixing form

--- src/components/SearchButton/index.tsx
+++ src/components/SearchButton/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./styles.module.scss";
 
 import { useTranslations } from "next-intl";
@@ -7,8 +6,10 @@ const SearchButton = () => {
   const t = useTranslations("Home");
   return (
     <div className={styles.search}>
-      <input type="text" placeholder={t("placeholder_search")} />
-      <button>{t("search_button")}</button>
+      <form>
+        <input type="text" placeholder={t("placeholder_search")} />
+        <button>{t("search_button")}</button>
+      </form>
     </div>
   );
 };
