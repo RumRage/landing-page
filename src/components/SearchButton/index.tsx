ae132016b7930c8a16d2e38b3e1cab3d295a9286
rumrage <claravillarruel4@gmail.com>
2024-02-15 12:44:10 -0300

    add new fuction

--- src/components/SearchButton/index.tsx
+++ src/components/SearchButton/index.tsx
@@ -1,13 +1,29 @@
-import styles from "./styles.module.scss";
+"use client";
 
+import styles from "./styles.module.scss";
+import { useState } from "react";
 import { useTranslations } from "next-intl";
 
 const SearchButton = () => {
   const t = useTranslations("Home");
+
+  const [email, setEmail] = useState("");
+
+  const handleClick = () => {
+    console.log("Email: ", email);
+  };
+
   return (
     <form className={styles.search}>
-      <input type="email" placeholder={t("placeholder_search")} />
-      <button>{t("search_button")}</button>
+      <input
+        type="email"
+        placeholder={t("placeholder_search")}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="button" onClick={handleClick}>
+        {t("search_button")}
+      </button>
     </form>
   );
 };
